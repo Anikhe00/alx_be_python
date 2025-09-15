9c8cb5deeecf51cd1a2c12753d23b0ca1133d4cd
Shakirat Akanji <akanjishakirato@gmail.com>
2025-09-15 17:48:10 +0100

    Learn to use Match Case Statements for handling multiple operations in a simple calculator program

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,24 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operator = input("Choose the operation (+, -, *, /): ")
+result = 0
+
+match operator:
+  case "+":
+    result = num1 + num2
+    print(f"The result is {result}")
+  case "-":
+    result = num1 - num2
+    print(f"The result is {result}")
+  case "*":
+    result = num1 * num2
+    print(f"The result is {result}")
+  case "/":
+    if num2 != 0:
+      result = round(num1 / num2, 2)
+      print(f"The result is {result}")
+    else:
+      print(f"Cannot divide by zero.")
+  case _:
+    print("Invalid operator.")
+
