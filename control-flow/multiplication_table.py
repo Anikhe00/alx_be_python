c28db70ef12cb8ed48d08e4917d8e8a87401a4d8
Shakirat Akanji <akanjishakirato@gmail.com>
2025-09-15 18:09:06 +0100

    Update the iterator range in multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,6 +2,6 @@
 number = int(input("Enter a number to see its multiplication table: "))
 
 # Use a for loop to generate the multiplication table
-for i in range(1, 115):
+for i in range(1, 11):
   product = number * i
   print(f"{number} x {i} = {product}", end="\t") 
