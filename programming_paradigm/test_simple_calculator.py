a9b388c57f25c3b369136c1291149aaacb412fb8
Shakirat Akanji <akanjishakirato@gmail.com>
2025-10-01 11:20:43 +0100

    Update test cases in test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -15,12 +15,12 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(10,2), 8)
         self.assertEqual(self.calc.subtract(-2, 5), -7)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(3,5), 15)
         self.assertEqual(self.calc.multiply(2,3), 6)
         self.assertEqual(self.calc.multiply(-1,1), -1)
     
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10,2), 5)
         self.assertEqual(self.calc.divide(5,2), 2.5)
         self.assertEqual(self.calc.divide(-10,2), -5)
